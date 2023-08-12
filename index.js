9201b8a9c97e2c65560fef817dd3011fe277b66d
Altenia Gaines <gaines.tina90@gmail.com>
2023-08-12 12:20:07 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
-  //add static methods here
+  constructor(string) {
+    this.string = string;
+  }
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\-\'\s]/g, "");
+  }
+  static titleize(string) {
+    const words = string.toLowerCase().split(" ");
+    const exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+      "is",
+    ];
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+    return titleizedWords.join(" ");
+  }
 }
+
+let newString = "hello the world is awesome";
+const capped = Formatter.titleize(newString);
+console.log(capped);
